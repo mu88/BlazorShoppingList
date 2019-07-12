6a28a6f6fd3a03a517cc3e8b188a9d72cf230b3b
mu88 <mu88_developer@outlook.com>
2019-07-12 15:28:27 +0200

    Add first code

---
+++ DTO/IngredientDto.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorShoppingList.DTO
+{
+    public class IngredientDto
+    {
+        public string Type { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
---
+++ DTO/RecipeDto.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace BlazorShoppingList.DTO
+{
+    public class RecipeDto
+    {
+        public RecipeDto()
+        {
+            Ingredients = new List<IngredientDto>();
+        }
+
+        public string Name { get; set; }
+
+        public List<IngredientDto> Ingredients { get; set; }
+    }
+}
